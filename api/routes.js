f4cc26f712274ef6fb8c70a58612ed5fe0b6201f
Raymond Findlay <raymond@automated-intelligence.com>
2020-11-30 09:54:16 +0000

    c

--- api/routes.js
+++ api/routes.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors");
+
+router.use(cors());
 
 let tasks = [
     {
-        id: 1,
-        description: "a task",
+        TaskId: 1,
+        TaskDefinition: "a task",
+        DueDate: new Date("1/1/2020"),
+        Priority: "High",
         isComplete: true
     },
     {
-        id: 2,
-        description: "another task",
-        isComplete: true
+        TaskId: 2,
+        TaskDefinition: "another task",
+        DueDate: new Date("1/3/2020"),
+        Priority: "High",
+        isComplete: false
+    },
+    {
+        TaskId: 3,
+        TaskDefinition: "a further task",
+        DueDate: new Date("1/3/2020"),
+        Priority: "High",
+        isComplete: false
     }
 ];
 
@@ -21,15 +35,19 @@ router.get('/', (req, res) => { //ok for now
 
 //GET :id
 router.get('/:id', (req, res) => {
-    //TODO
+    const id = parseInt(req.params.id);
+    const targetTask = tasks.find((task) => task.TaskId === id);
+    res.json(targetTask);
 });
 
 //POST
-router.post('/', (req, res) => { // ok for now
+router.post('/', (req, res) => {
     const newTask = {
-        id: req.body.id,
-        description: req.body.description,
-        isComplete: req.body.isComplete
+        TaskId: parseInt(req.body.TaskId),
+        TaskDefinition: req.body.TaskDefinition,
+        DueDate: req.body.DueDate,
+        Priority: req.body.Priority,
+        isComplete: req.body.isComplete === "false" ? false : true
     };
 
     tasks.push(newTask);
@@ -38,12 +56,30 @@ router.post('/', (req, res) => { // ok for now
 
 //PUT :id
 router.put('/:id', (req, res) => {
-    //TODO
+    const {
+        TaskDefinition,
+        DueDate,
+        Priority,
+        isComplete
+    } = req.body; // This is called object destructuring
+
+    const id = parseInt(req.params.id);
+    let targetTask = tasks.find((task) => task.TaskId === id);
+
+    // assign updated values
+    targetTask.TaskDefinition = TaskDefinition;
+    targetTask.DueDate = DueDate;
+    targetTask.Priority = Priority;
+    targetTask.isComplete = isComplete === "false" ? false : true;
+
+    res.json(tasks);
 });
 
 //DELETE
 router.delete('/:id', (req, res) => {
-    //TODO
+    const id = parseInt(req.params.id);
+    tasks = tasks.filter((task) => task.TaskId !== id);
+    res.json(tasks);
 });
 
 module.exports = router;
